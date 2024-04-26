4f7d686c1f92532e21773a36a4444dc61ce003da
Vicente Castro de Oliveira <vicente-castro97@hotmail.com>
2024-04-26 18:08:13 -0300

    Update Usuario.java
    atualizaçao do construtor e metodos da classe usuario

--- Usuario.java
+++ Usuario.java
@@ -1,3 +1,24 @@
 public class Usuario {
+private int id;
+private String nome;
 
+private Boolean tipo;
+
+    public Usuario(int id, String nome, Boolean tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Boolean getTipo() {
+        return tipo;
+    }
 }
