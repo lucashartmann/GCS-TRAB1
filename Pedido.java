6bf96337ff270a72f7bca84185cf311955e9844f
Lucas Augusto Hartmann <lucas.a111@hotmail.com>
2024-06-07 23:06:46 -0300

    Construtor criado

--- Pedido.java
+++ Pedido.java
@@ -10,4 +10,15 @@ public class Pedido {
     private String status;
     private List<Item> itens;
     private double valorTotal;
+
+    public Pedido(Usuario solicitante, Departamento departamento, Date dataPedido, Date dataConclusao,
+            String status, List<Item> itens, double valorTotal) {
+        this.solicitante = solicitante;
+        this.departamento = departamento;
+        this.dataPedido = dataPedido;
+        this.dataConclusao = dataConclusao;
+        this.status = status;
+        this.itens = itens;
+        this.valorTotal = valorTotal;
+    }
 }
