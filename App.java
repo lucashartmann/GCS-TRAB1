f51eef9c38c5b2e17b2376b9c5d16929bb8a5bcb
Lucas Augusto Hartmann <lucas.a111@hotmail.com>
2024-06-07 23:21:34 -0300

    Método inicializar criado. Ele cria objetos das classes

--- App.java
+++ App.java
@@ -11,4 +11,23 @@ public class App {
         selecionarUsuarioAtual();
         menu();
     }
+
+    private void inicializar() {
+        Departamento Financeiro = new Departamento("Financeiro", 1000.0);
+        Departamento RH = new Departamento("RH", 1500.0);
+        Departamento Engenharia = new Departamento("Engenharia", 2000.0);
+        Departamento Manutencao = new Departamento("Manutenção", 1200.0);
+        Departamento TI = new Departamento("TI", 1800.0);
+        sistema.adicionarDepartamento(TI);
+        sistema.adicionarDepartamento(RH);
+        sistema.adicionarDepartamento(Financeiro);
+        sistema.adicionarDepartamento(Engenharia);
+        sistema.adicionarDepartamento(Manutencao);
+        Usuario Alice = new Usuario(1, "Alice", false, Financeiro);
+        Usuario Bob = new Usuario(2, "Bob", false, TI);
+        Usuario Carol = new Usuario(3, "Carol", true, Manutencao);
+        sistema.adicionarUsuario(Alice);
+        sistema.adicionarUsuario(Bob);
+        sistema.adicionarUsuario(Carol);
+    }
 }
