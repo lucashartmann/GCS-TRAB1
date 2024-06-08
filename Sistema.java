b3e273b078da4e93f21dd12524ebdb6e7563d973
Lucas Augusto Hartmann <lucas.a111@hotmail.com>
2024-06-07 23:11:33 -0300

    Construtor e gets criados

--- Sistema.java
+++ Sistema.java
@@ -4,4 +4,18 @@ public class Sistema {
     private List<Usuario> usuarios;
     private List<Pedido> pedidos;
     private List<Departamento> departamentos;
+
+    public Sistema() {
+        this.departamentos = new ArrayList<>();
+        this.usuarios = new ArrayList<>();
+        this.pedidos = new ArrayList<>();
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
 }
