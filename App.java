9f65c77f01b5fd2299df92bf703e3f4675b37e0e
Lucas Augusto Hartmann <lucas.a111@hotmail.com>
2024-06-07 23:28:38 -0300

    Imports adicionais. Classe finalizada

--- App.java
+++ App.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.InputMismatchException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
