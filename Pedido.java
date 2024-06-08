bd3298912e356853705ac0c6b16b7cd2dd7027f7
Lucas Augusto Hartmann <lucas.a111@hotmail.com>
2024-06-07 23:07:36 -0300

    Métodos get e set implementados

--- Pedido.java
+++ Pedido.java
@@ -1,7 +1,6 @@
 import java.util.Date;
 import java.util.List;
 
-
 public class Pedido {
     private Usuario solicitante;
     private Departamento departamento;
@@ -21,4 +20,69 @@ public class Pedido {
         this.itens = itens;
         this.valorTotal = valorTotal;
     }
+
+    public Usuario getSolicitante() {
+        return solicitante;
+    }
+
+    public void setSolicitante(Usuario solicitante) {
+        this.solicitante = solicitante;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public Date getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(Date dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public Date getDataConclusao() {
+        return dataConclusao;
+    }
+
+    public void setDataConclusao(Date dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Item> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<Item> itens) {
+        this.itens = itens;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido [solicitante=" + solicitante.getNome() + ", departamento=" + departamento.getNome()
+                + ", dataPedido=" + dataPedido
+                + ", dataConclusao=" + dataConclusao + ", status=" + status + ", itens=" + itens + ", valorTotal="
+                + valorTotal + "]";
+    }
+
 }
